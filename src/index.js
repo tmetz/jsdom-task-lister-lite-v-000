c1ae9f614c1d2184045baab9e787d179c3ab518d
Tammy Metz <tammy@tammymetz.com>
2019-10-05 01:29:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-new-task-form").addEventListener("submit", addTask);
+  
 });
+
+const addTask = event => {
+  event.preventDefault();
+  const newTaskList = document.getElementById("tasks");
+  const desc = document.getElementById("new-task-description");
+  const task = document.createElement("li");
+  task.innerText = desc.value;
+  newTaskList.appendChild(task);
+}
